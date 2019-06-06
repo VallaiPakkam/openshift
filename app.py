83115210679d106feedc5a52c6da0580dfeb540e
arun <arunease@gmail.com>
2019-06-06 20:24:01 +0200

    remove a extra route

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
-def main():
-    return "I am good, how about you?"
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
